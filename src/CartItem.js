db67b0a5423a3614e3df73c60ba46baae9dc922f
thearyan007 <thearyangupta007@gmail.com>
2023-10-03 11:40:06 +0530

    added increase and decrease functionality

--- src/CartItem.js
+++ src/CartItem.js
@@ -7,8 +7,22 @@ class CartItem extends React.Component{
             price: 15999,
             title: "Phone",
             Qty: 1,
-            img:""
+            img:"",
         }
+        // this.increaseQty= this.increaseQty.bind(this);
+    }
+    increaseQty=() => {
+        this.setState({
+            Qty: this.state.Qty+1
+        })
+    }
+    decreaseQty=()=>{
+        this.setState((preState)=>{
+            return {
+                Qty: preState.Qty-1
+            }
+        })
+
     }
 
     render(){
@@ -23,8 +37,8 @@ class CartItem extends React.Component{
                     <div style={{color:'#777'}}>Price: {price}</div>
                     <div style={{color:'#777'}}>Qty: {Qty}</div>
                     <div className="cart-item-actions">
-                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/262/262038.png"/>
-                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/659/659892.png"/>
+                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/262/262038.png" onClick={this.increaseQty}/>
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/659/659892.png" onClick={this.decreaseQty}/>
                         <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"/>
 
                     </div>
