685bb354d224188a4ad6d3f9e8b50be7bfd3d77a
thearyan007 <thearyangupta007@gmail.com>
2023-10-06 00:00:51 +0530

    connected Firebase

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React from "react";
 import Cart from "./Cart";
 import Navbar from "./Navbar";
-
+import {getFirestore, collection, getDocs}  from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
 
 class App extends React.Component {
   constructor(){
     super();
     this.state={
-        products:[{
-            price: 45999,
-            title: "Phone",
-            Qty: 1,
-            img:"https://www.91-img.com/pictures/143993-v4-apple-iphone-14-mobile-phone-large-4.jpg",
-            id:1
-        },{
-            price: 5999,
-            title: "watch",
-            Qty: 1,
-            img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9fVUBh01LEW5149n-FRuY7wT2t01qVwQrWemCzgrVlHGHNrX4PIowl-FyoS0GvT92lAs&usqp=CAU",
-            id:2
-
-        },{
-            price: 999,
-            title: "charger",
-            Qty: 1,
-            img:"https://m.media-amazon.com/images/I/61NfFCwsneL._AC_UF1000,1000_QL80_.jpg",
-            id:3
-
-        }
-    ]
+        products:[],
+        loading:true
     }
     
 }
@@ -81,8 +62,41 @@ totalCartPrice=()=>{
 }
 
 
+componentDidMount(){
+    // firebase
+    // .firestore()
+    // .collection("products")
+    // .get().then((snapshot)=>{
+    //     console.log(snapshot)
+    // })
+    const db = getFirestore();
+    const colRef = collection(db, "products")
+    getDocs(colRef).then((snapshot)=>{
+
+        snapshot.docs.forEach((doc)=>{
+            console.log(doc.data())
+        })
+
+        const products =snapshot.docs.map((doc)=>{
+            const data= doc.data();
+            data['id']=doc.id;
+
+            return data
+        })
+
+        this.setState({
+            products,
+            loading:false
+            
+        })
+    }).catch(err=>{
+        console.log(err.message)
+    })
+
+}
+
   render(){
-    const {products} = this.state;
+    const {products, loading} = this.state;
     return (
       <div className="App">
         {/* <h1>Cart</h1> */}
@@ -93,6 +107,7 @@ totalCartPrice=()=>{
           onDecreaseQuantity={this.handleDecreaseQty}
           onDeleteItem={this.deleteItem}
         />
+        {loading && <h1>Loading products...</h1>}
         <div style={ {padding: 10, fontSize: 20} }>
             Total Cost: ₹{this.totalCartPrice()}
         </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAFZlqMYLNaWA3bv3dknfsDxGFS6RwJzqs",
+    authDomain: "cart-6faec.firebaseapp.com",
+    projectId: "cart-6faec",
+    storageBucket: "cart-6faec.appspot.com",
+    messagingSenderId: "530793448762",
+    appId: "1:530793448762:web:ba8ca6f9c534ba88867c03"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById("root"))
 
